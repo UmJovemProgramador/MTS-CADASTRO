c56d7f035731b81376d152f6190aede478850bcd
Antônio Lucas C Araújo <112925082+UmJovemProgramador@users.noreply.github.com>
2023-10-10 22:42:28 -0300

    Add files via upload

---
+++ assets/js/cadastro.js
@@ -0,0 +1,29 @@
+function cadastrar() {
+
+  var nome = document.getElementById('nome-input').value;
+  var cpf = document.getElementById('cpf-input').value;
+  var senha = document.getElementById('senha-input').value;
+
+  if (cpf === '' || senha === '' || nome === '') {
+    alert('Por favor, preencha todos os campos antes de cadastrar.');
+  } else {
+    // Redirecionar para a página desejada após o cadastro bem-sucedido
+    var link = '../../login.html'; 
+    window.location.href = link;
+
+  }
+}
+
+function login() {
+
+  var cpf = document.getElementById('cpf-input').value;
+  var senha = document.getElementById('senha-input').value;
+
+  if (cpf=== '' || senha === '') {
+    alert('Por favor, preencha todos os campos antes de cadastrar.');
+  } else {
+    // Redirecionar para a página desejada após o cadastro bem-sucedido
+    var link = 'https://umjovemprogramador.github.io/MTS-2.0/'; 
+    window.location.href = link;
+  }
+}
