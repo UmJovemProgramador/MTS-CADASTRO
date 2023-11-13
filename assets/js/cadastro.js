f7e5b55a4c196e0977142dbae86b76a3842e6b80
Antônio Lucas C Araújo <112925082+UmJovemProgramador@users.noreply.github.com>
2023-11-13 16:06:39 -0300

    Add files via upload

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -8,9 +8,21 @@ function cadastrar() {
     alert('Por favor, preencha todos os campos antes de cadastrar.');
   } else {
     // Redirecionar para a página desejada após o cadastro bem-sucedido
-    var link = '../../login.html'; 
-    window.location.href = link;
-
+    fetch('http://localhost:3000/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nome, cpf, senha }),
+    }).then(data => {
+      console.log('Resposta do servidor:', data);
+      // Trate a resposta do servidor conforme necessário
+      var link = '../../login.html'; 
+      window.location.href = link;
+    })
+    .catch(error => {
+      console.error('Erro ao enviar dados para o servidor:', error);
+    });
   }
 }
 
@@ -27,3 +39,4 @@ function login() {
     window.location.href = link;
   }
 }
+
