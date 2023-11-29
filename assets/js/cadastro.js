6cf92bb5238b82ac5ad548928e50e12bfbfa8982
Antônio Lucas C Araújo <112925082+UmJovemProgramador@users.noreply.github.com>
2023-11-28 21:31:22 -0300

    Update cadastro.js

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -23,7 +23,7 @@ function login() {
     alert('Por favor, preencha todos os campos antes de cadastrar.');
   } else {
     // Redirecionar para a página desejada após o cadastro bem-sucedido
-    var link = 'https://umjovemprogramador.github.io/MTS-2.0/'; 
+    var link = 'https://mts-4-0.vercel.app/'; 
     window.location.href = link;
   }
 }
